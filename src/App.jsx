d80be5b0a2d71a836b605b22ed607a32a871e541
dyprodg <dennisdiepolder@outlook.com>
2023-05-29 16:35:31 +0200

    update2

--- src/App.jsx
+++ src/App.jsx
@@ -1,78 +1,89 @@
-/* eslint-disable no-unused-vars */
-import React from "react";
-import { motion } from "framer-motion";
-import logo from './logo.png';
-import GrainEffect from './GrainEffect'; // Stelle sicher, dass der Pfad korrekt ist
+import React, { useState, useEffect } from "react";
+import Preloader from "./Preloader";
+import { motion, AnimatePresence } from "framer-motion";
+import GrainEffect from './GrainEffect';
 import simon from './simon.png';
 import NavigationBar from "./Navbar";
-
 
 const FadeInAnimation = {
   hidden: { opacity: 0 },
   visible: { opacity: 1 },
+  exit: { opacity: 0 }
 };
 
-const handleButtonClick = () => {
-  window.open('https://api.whatsapp.com/send?phone=+4916042069696969', '_blank');
-};
+const Home = () => {
+  const [isAppLoaded, setIsAppLoaded] = useState(false);
 
+  useEffect(() => {
+    setTimeout(() => {
+      setIsAppLoaded(true);
+    }, 2000); 
+  }, []);
 
-const LandingPage = () => {
   return (
-    <div className="flex flex-col h-screen" style={{ backgroundColor: '#15180f', color: 'rgba(156, 77, 38, 1)', width: "100vw", position: "relative" }}>
-      <NavigationBar />
-      <GrainEffect />
-      
-      <motion.div 
-        className="text-4xl text-center my-auto"
-        initial="hidden"
-        animate="visible"
-        variants={FadeInAnimation}
-        transition={{ delay: 0.2, duration: 1.5 }}
-      >
-        <h1 className="my-0">
-          BEARSON
-        </h1>  
-      </motion.div>
-        <motion.div className="flex justify-center items-center h-screen">
-          <motion.img 
-          src={simon} 
-          alt="Simon" 
-          className="w-72 h-auto mx-auto my-0"
+    <AnimatePresence>
+      {!isAppLoaded && <Preloader />}
+      {isAppLoaded && (
+        <motion.div
+          className="flex flex-col h-screen"
+          style={{ color: "#9C4D26", width: "100vw", position: "relative" }}
           initial="hidden"
           animate="visible"
+          exit="exit"
           variants={FadeInAnimation}
-          transition={{ delay: 0.2, duration: 1.5 }}
-          /></motion.div>
-      <motion.div 
-        className="text-4xl text-center my-auto"
-        initial="hidden"
-        animate="visible"
-        variants={FadeInAnimation}
-        transition={{ delay: 0.4, duration: 1.5 }}> 
-        <h2 className="my-0">
-          TATTOO
-        </h2>
-      </motion.div>
-
-      <div className="flex justify-between items-end p-4">
-        <motion.a
-           className="px-4 py-2 bg-transparent rounded"
-           initial="hidden"
-           animate="visible"
-           variants={FadeInAnimation}
-           transition={{ delay: 1, duration: 2 }}
-           whileHover={{ scale: 1.1 }}
-           whileTap={{ scale: 0.9 }}
         >
-          ©bearsontattoo
-        </motion.a>
-        
-      </div>
-    </div>
+          <NavigationBar />
+          <GrainEffect />
+
+          <motion.div
+            className="text-4xl text-center"
+            initial="hidden"
+            animate="visible"
+            variants={FadeInAnimation}
+            transition={{ delay: 0.2, duration: 1.5 }}
+          >
+            <h1 className="my-0">BEARSON</h1>
+          </motion.div>
+
+          <motion.div className="flex justify-center items-center h-screen background">
+            <motion.img
+              src={simon}
+              alt="Simon"
+              className="w-72 h-auto mx-auto my-0"
+              initial="hidden"
+              animate="visible"
+              variants={FadeInAnimation}
+              transition={{ delay: 0.2, duration: 1.5 }}
+            />
+          </motion.div>
+
+          <motion.div
+            className="text-4xl text-center my-auto"
+            initial="hidden"
+            animate="visible"
+            variants={FadeInAnimation}
+            transition={{ delay: 0.4, duration: 1.5 }}
+          >
+            <h2 className="my-0">TATTOO</h2>
+          </motion.div>
+
+          <div className="flex justify-between items-end p-4">
+            <motion.a
+              className="px-4 py-2 bg-transparent rounded"
+              initial="hidden"
+              animate="visible"
+              variants={FadeInAnimation}
+              transition={{ delay: 1, duration: 2 }}
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+            >
+              ©bearsontattoo
+            </motion.a>
+          </div>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
-}
+};
 
-export default LandingPage;
-
-
+export default Home;
---
+++ src/Preloader.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import PuffLoader from "react-spinners/PuffLoader";
+import GrainEffect from './GrainEffect';
+
+const Preloader = () => {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+        setLoading(false);
+    }, 2000);
+  }, []);
+
+  return (
+    <div className="Preloader">
+    {
+      loading ?
+      (
+        <>
+          <GrainEffect />
+          <PuffLoader 
+            size={300}
+            color={"#9C4D26"}
+            loading={loading}
+          />
+        </>
+      )
+      : null
+    }
+    </div>
+  );
+};
+
+export default Preloader;
